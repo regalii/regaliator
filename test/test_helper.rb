7bd0c0b4293193fc2afd8bf102f2f0ee81ea547c
Hesham <elnahhas@gmail.com>
2016-01-21 17:13:04 -0500

    Fix conflict

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
   c.hook_into :webmock
-<<<<<<< HEAD
   c.before_record do |i|
     i.response.body.force_encoding('UTF-8')
   end
-=======
->>>>>>> develop
 end
