709d1e7acd81a94233416a757f5c15af47411bb2
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-01-13 09:20:30 -0600

    remove unnecessary initialization

--- lib/regaliator/configuration.rb
+++ lib/regaliator/configuration.rb
@@ -5,18 +5,10 @@ module Regaliator
     attr_reader   :version
 
     def initialize
-      @api_key      = nil
-      @secret_key   = nil
-      @host         = nil
       @version      = Regaliator::API_VERSION
       @open_timeout = 10
       @read_timeout = 60
       @use_ssl      = true
-
-      @proxy_host   = nil
-      @proxy_port   = nil
-      @proxy_user   = nil
-      @proxy_pass   = nil
     end
 
     def secure?
