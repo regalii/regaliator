4760947b8c7e5e5aef8fb5a3ea52b825276afe34
Geoffrey Roguelon <geoffrey@regalii.com>
2017-01-16 15:49:59 -0600

    Use the default configuration in Client.

--- lib/regaliator/client.rb
+++ lib/regaliator/client.rb
@@ -2,7 +2,7 @@ module Regaliator
   class Client
     attr_accessor :config
 
-    def initialize(config)
+    def initialize(config = Regaliator.configuration)
       @config = config
     end
   end
