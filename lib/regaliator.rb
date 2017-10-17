8bf5478d34d88b1ec3c2ce09c5480d59408766e8
rlopez <romariolopezc@gmail.com>
2017-10-17 16:10:38 -0500

    Remove usage of versioned modules

--- lib/regaliator.rb
+++ lib/regaliator.rb
@@ -1,18 +1,7 @@
 require 'regaliator/configuration'
-require 'regaliator/api_version_error'
-require 'regaliator/v15'
-require 'regaliator/v30'
-require 'regaliator/v31'
-require 'regaliator/v32'
+require 'regaliator/client'
 
 module Regaliator
-  API_VERSIONS = {
-    V15::API_VERSION => V15::Client,
-    V30::API_VERSION => V30::Client,
-    V31::API_VERSION => V31::Client,
-    V32::API_VERSION => V32::Client
-  }.freeze
-
   class << self
     def configuration
       @config ||= Configuration.new
@@ -26,11 +15,7 @@ module Regaliator
       config = get_configuration(arg)
       yield(config) if block_given?
 
-      unless API_VERSIONS.key?(config.version)
-        raise APIVersionError.new(config.version)
-      end
-
-      API_VERSIONS[config.version].new(config)
+      Client.new(config)
     end
 
     def method_missing(method_name, *args)
--- lib/regaliator/api_version_error.rb
+++
@@ -1,7 +0,0 @@
-module Regaliator
-  class APIVersionError < StandardError
-    def initialize(api_version)
-      super("API version `#{api_version}' isn't supported!")
-    end
-  end
-end
